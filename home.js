2d964ef817eae9055a7c6b4476427414ddcecda5
404nafiz <akm.nafiz.ibn.nasim.bd@gmail.com>
2025-09-06 23:23:15 +0600

    transactions function   add

--- home.js
+++ home.js
@@ -13,32 +13,30 @@ const numberInput = document.getElementById("number");
 const mainAmountElement = document.getElementById("mainammount");
 const amountInput = document.getElementById("Amount");
 const bankSelection = document.getElementById("bankSelection");
-const TSSubmit=document.getElementById("TSSubmit");
-const paysubmit=document.getElementById("paysubmit");
-const BonusSubmit=document.getElementById("BonusSubmit");
-let datas=[ ];
+const TSSubmit = document.getElementById("TSSubmit");
+const paysubmit = document.getElementById("paysubmit");
+const BonusSubmit = document.getElementById("BonusSubmit");
+const transdiv=document.getElementById("transdiv")
+let datas = [];
 
 
 // for the verification of the account number 
-function VerificationOfNumber(id)
-{
-    const vall=document.getElementById(id).value;
-   
+function VerificationOfNumber(id) {
+    const vall = document.getElementById(id).value;
 
-     if (vall.length !== 11 || isNaN(vall)) {
+
+    if (vall.length !== 11 || isNaN(vall)) {
         console.error("Account number must be 11 digits.");
         alert("Account number must be 11 digits.");
         return;
     }
 }
 // for the verification of the pin
-function VerificationOfPin(id)
-{
-    const pin=document.getElementById(id).value;
-    if(pin.length!==4)
-    {
+function VerificationOfPin(id) {
+    const pin = document.getElementById(id).value;
+    if (pin.length !== 4) {
         alert("PIN must be 4 digits.")
-return false;
+        return false;
     }
     return true;
 
@@ -46,74 +44,90 @@ return false;
 
 }
 // for the sum of tk
-function addtk(documentofaddmoney)
-{
+function addtk(documentofaddmoney) {
 
 
-let tk = parseInt(document.getElementById("mainammount").innerHTML);
-    
-let addtk=parseInt(document.getElementById(documentofaddmoney).value);
-tk=tk+addtk;
-document.getElementById("mainammount").innerHTML=tk;
+    let tk = parseInt(document.getElementById("mainammount").innerHTML);
+
+    let addtk = parseInt(document.getElementById(documentofaddmoney).value);
+    tk = tk + addtk;
+    document.getElementById("mainammount").innerHTML = tk;
 
 }
 
-function substractiontk(documentofsubmoney)
-{
+function substractiontk(documentofsubmoney) {
+    let tk = parseInt(document.getElementById("mainammount").innerHTML);
 
-
-let tk = parseInt(document.getElementById("mainammount").innerHTML);
-    
-let addtk=parseInt(document.getElementById(documentofsubmoney).value);
-tk=tk-addtk;
-document.getElementById("mainammount").innerHTML=tk;
+    let addtk = parseInt(document.getElementById(documentofsubmoney).value);
+    tk = tk - addtk;
+    document.getElementById("mainammount").innerHTML = tk;
 
 }
 // array that carry the all the div value that i have to change the for the hidden and toggle
-const divs=[addmoneydiv,cashoutdiv,Transferdiv,Bonusdiv,transdiv,paybilldiv];
-function toggleDivVisibility(targetDiv){
+const divs = [addmoneydiv, cashoutdiv, Transferdiv, Bonusdiv, transdiv, paybilldiv];
+function toggleDivVisibility(targetDiv) {
 
-divs.forEach(div=> div.classList.add("hidden"));
-targetDiv.classList.toggle("hidden");
+    divs.forEach(div => div.classList.add("hidden"));
+    targetDiv.classList.toggle("hidden");
 
 }
 
 // add money option
 addmoney.addEventListener('click', function (e) {
     e.preventDefault();
-   toggleDivVisibility(addmoneydiv)
+    toggleDivVisibility(addmoneydiv)
 
 })
 // cashout money option
 cashout.addEventListener('click', function (e) {
     e.preventDefault();
 
- toggleDivVisibility(cashoutdiv)
+    toggleDivVisibility(cashoutdiv)
 
 })
 // transfer money option
-Transfer.addEventListener('click',function(e){
+Transfer.addEventListener('click', function (e) {
     e.preventDefault();
     toggleDivVisibility(Transferdiv)
 })
 // get Bonus option
-bonus.addEventListener('click',function(e){
+bonus.addEventListener('click', function (e) {
     e.preventDefault();
     toggleDivVisibility(Bonusdiv)
 })
 // pay bill option
 
-paybill.addEventListener('click',function(e){
+paybill.addEventListener('click', function (e) {
     e.preventDefault();
     toggleDivVisibility(paybilldiv)
 })
 // transactions
-transactions.addEventListener('click',function(e){
+transactions.addEventListener('click', function (e) {
     e.preventDefault();
     toggleDivVisibility(transdiv)
 
-
+for (const element of datas) {
+const div=document.createElement("div");
+div.innerHTML=`  <div
+      id="historydiv"
+      class="form mx-5 sm:mx-0 sm:px-5 flex flex-row gap-5 mt-5 items-center bg-[#a2bbee] py-5 rounded-2xl px-2"
+    >
+    <div class="left bg-[#0808082d] p-3 rounded-3xl">
+        <img src="assets/wallet1.png"" alt="" ">
+      </div>
+      <div class="right">
+        <h3 class="historyhead">${element.name}</h3>
+        <p class="historytime">${element.time}</p>
+      </div>
+     
+    </div>`
     
+transdiv.appendChild(div)
+}
+   console.log(datas[0].time,datas[0].name);
+   
+
+
 
 })
 // submit button for the add money
@@ -132,7 +146,7 @@ submit.addEventListener('click', function (event) {
         return;
     }
 
-    
+
     if (pin.length !== 4 || isNaN(pin)) {
         console.error("PIN must be 4 digits.");
         alert("PIN must be 4 digits.");
@@ -155,12 +169,12 @@ submit.addEventListener('click', function (event) {
     addtk("Amount");
 
 
-    const data={
-  name:"Add Money",
-  time:Date().toLocaleTimeString()
+    const data = {
+        name: "Add Money",
+        time: Date().toLocaleTimeString()
 
-}
-datas.push(data);
+    }
+    datas.push(data);
 });
 // for Cash out
 const CoSubmit = document.getElementById("CoSubmit");
@@ -204,57 +218,55 @@ CoSubmit.addEventListener('click', function (event) {
 
     const totalBalance = mainAmount - amount;
     mainAmountElement.innerText = totalBalance;
-        const data={
-  name:"Cashout Money",
-  time:Date().toLocaleTimeString()
+    const data = {
+        name: "Cashout Money",
+        time: new Date().toLocaleTimeString()
 
-}
-datas.push(data);
+    }
+    datas.push(data);
 });
 
 // transfer section
-TSSubmit.addEventListener('click',function(e){
-e.preventDefault();
-VerificationOfNumber("TSNumber");
-VerificationOfPin("TSPin");
-substractiontk("TSAmount");
-    const data={
-  name:"Transfer Money",
-  time:Date().toLocaleTimeString()
+TSSubmit.addEventListener('click', function (e) {
+    e.preventDefault();
+    VerificationOfNumber("TSNumber");
+    VerificationOfPin("TSPin");
+    substractiontk("TSAmount");
+    const data = {
+        name: "Transfer Money",
+        time: new Date().toLocaleTimeString()
 
-}
-datas.push(data);
+    }
+    datas.push(data);
 })
 
 
 // Bonus
-BonusSubmit.addEventListener('click',function(e)
-{
+BonusSubmit.addEventListener('click', function (e) {
     e.preventDefault();
-    if(VerificationOfPin("BonusPin"))
-    {
+    if (VerificationOfPin("BonusPin")) {
         let tk = parseInt(document.getElementById("mainammount").innerHTML);
-    
 
-tk=parseInt(tk+500);
-document.getElementById("mainammount").innerHTML=tk;
+
+        tk = parseInt(tk + 500);
+        document.getElementById("mainammount").innerHTML = tk;
     }
-    
+
 
 
 })
 // pay now
-paysubmit.addEventListener('click',function(e){
+paysubmit.addEventListener('click', function (e) {
     e.preventDefault();
     VerificationOfNumber("payaccountnumber");
-VerificationOfPin("paypin");
-substractiontk("payAmount");
-    const data={
-  name:"Pay Money",
-  time:Date().toLocaleTimeString()
+    VerificationOfPin("paypin");
+    substractiontk("payAmount");
+    const data = {
+        name: "Pay Money",
+        time: new Date().toLocaleTimeString()
 
-}
-datas.push(data);
+    }
+    datas.push(data);
 
 })
 
